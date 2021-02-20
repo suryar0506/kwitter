dcbaf6aafa3a7021383e791a2d129bc77d2c9abf
suryar0506 <72110042+suryar0506@users.noreply.github.com>
2021-02-21 02:22:04 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,15 +1,16 @@
 
 //ADD YOUR FIREBASE LINKS HERE
-  var firebaseConfig = {
-      apiKey: "AIzaSyD3R6zKhpl4UQcqQm6AVOdSNaXtSze15FA",
-      authDomain: "kwitter-c71ca.firebaseapp.com",
-      databaseURL: "https://kwitter-c71ca-default-rtdb.firebaseio.com",
-      projectId: "kwitter-c71ca",
-      storageBucket: "kwitter-c71ca.appspot.com",
-      messagingSenderId: "774546883472",
-      appId: "1:774546883472:web:048a79d87ff6c94fb12915",
-      measurementId: "G-QG4QSK5RCF"
+var firebaseConfig = {
+      apiKey: "AIzaSyCNNU4HHcvVXLB1QkqRDfls5N53lDCJwoQ",
+      authDomain: "kwitter-project-cdc06.firebaseapp.com",
+      databaseURL: "https://kwitter-project-cdc06-default-rtdb.firebaseio.com",
+      projectId: "kwitter-project-cdc06",
+      storageBucket: "kwitter-project-cdc06.appspot.com",
+      messagingSenderId: "69802262594",
+      appId: "1:69802262594:web:8ae0dba48763e4a6946ddb"
     };
+    firebase.initializeApp(firebaseConfig);
+
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Start code
